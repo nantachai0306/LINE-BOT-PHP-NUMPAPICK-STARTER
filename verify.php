3193594110761ac6a7f29acaa2e8f5092f89303a
nantachai0306 <44888272+nantachai0306@users.noreply.github.com>
2018-12-10 15:23:41 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ZgTrbsRBYxmyubKP1G7rhpL4bzTTXQlMvdhlLfIs9nW/YiUalHH1d/wAxlpIJGpYuGQ4qDg9zll9/XQN0Xp2rkuDAn+cBVf5gkm8mTqNSh8JYo9P6CWRuvokXwW';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
