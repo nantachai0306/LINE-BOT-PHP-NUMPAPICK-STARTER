b44262e733799642ba3dadcc2bd772bcf7fc58e8
nantachai0306 <44888272+nantachai0306@users.noreply.github.com>
2018-12-10 17:21:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = '6glFwNzCJWIywIhdtMq1kvJMWv7umWrB18oDUgVKQ6hCkLc1PAaEO/WSl+gc/NEbuGQ4qDg9zll9/XQN0Xp2rkuDAn+cBVf5gkm8mTqNSh9LKgKqnyzRSJj5LJMv9JpVZU0hLRanSLl1XFpuwKXRPQdB04t89/1O/w1cDnyilFU=';
 
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v4/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
